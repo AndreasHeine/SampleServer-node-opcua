f4245b74329a31c1362a371faf683fcca8f51728
Andreas <info@andreas-heine.net>
2021-12-01 11:46:19 +0100

    test

---
+++ src/test.ts
@@ -0,0 +1,10 @@
+import { describe, it } from "mocha"
+import { expect } from "chai"
+
+describe('Tests', () => {
+    describe('Test1', () => {
+        it('should pass', () => {
+            //
+        })
+    })
+  })
