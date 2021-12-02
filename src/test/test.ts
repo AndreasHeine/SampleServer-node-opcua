8264a4d390438286ae282841cb54bcf369ecaf1f
Andreas <info@andreas-heine.net>
2021-12-02 09:54:11 +0100

    add: startup test

--- src/test/test.ts
+++ src/test/test.ts
@@ -1,10 +1,46 @@
+// Copyright 2021 (c) Andreas Heine
+//
+//   Licensed under the Apache License, Version 2.0 (the 'License');
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an 'AS IS' BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
 import { describe, it } from "mocha"
 import { expect } from "chai"
 
-describe('Tests', () => {
-    describe('Test1', () => {
-        it('should pass', () => {
-            //
-        })
+import { 
+    OPCUAServer, 
+} from 'node-opcua'
+
+import { config } from "./../config"
+
+describe('Tests:', () => {
+    let server: OPCUAServer
+    beforeEach((done) => {
+        server = new OPCUAServer(config)
+        server.initialize()
+        server.start(() => {
+            done()
+        });
+    });
+    afterEach((done) => {
+        if (server) {
+            server.shutdown(() => {
+                done()
+            });
+        } else {
+            done()
+        }
+    });
+
+    it('should start the OPCUAServer', () => { 
+        //        
     })
-  })
+})
