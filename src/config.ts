006671fb85c46c5fff995fb64baec63b70e3bdcd
Andreas <info@andreas-heine.net>
2022-01-29 11:59:38 +0100

    add missing config params

--- src/config.ts
+++ src/config.ts
@@ -33,24 +33,24 @@ const argv = yargs(process.argv.slice(2)).options({
   configpath: { type: 'string' }
 }).parseSync();
 
-const configPath: string = process.env.CONFIGPATH || argv.configpath || "";
-const configFile: string = "configuration.json";
+const configPath: string = process.env.CONFIGPATH || argv.configpath || '';
+const configFile: string = 'configuration.json';
 
 let configJsonObj: OPCUAServerOptions;
 try {
-  const configString = readFileSync(configPath + configFile, "utf-8");
+  const configString = readFileSync(configPath + configFile, 'utf-8');
   configJsonObj = JSON.parse(configString) || {};
 } catch (error) {
   red(`Error while loading config-file: ${error}`);
   configJsonObj = {};
 };
 
-const packageString: string = readFileSync("package.json", "utf-8");
+const packageString: string = readFileSync('package.json', 'utf-8');
 const packageJsonObj = JSON.parse(packageString) || {};
 
 let config: OPCUAServerOptions = {};
 
-config.hostname = process.env.IP || argv.ip || configJsonObj.hostname || "127.0.0.1";
+config.hostname = process.env.IP || argv.ip || configJsonObj.hostname || '127.0.0.1';
 
 if (!configJsonObj.port) {
   config.port = Number(process.env.PORT) || argv.port || 4840;
@@ -69,7 +69,18 @@ config.alternateEndpoints = configJsonObj.alternateEndpoints || [];
 config.maxAllowedSessionNumber = configJsonObj.maxAllowedSessionNumber || 100;
 config.maxConnectionsPerEndpoint = configJsonObj.maxConnectionsPerEndpoint || 100;
 config.timeout = configJsonObj.timeout || 10000;
-config.resourcePath = configJsonObj.resourcePath || "/UA";
+config.resourcePath = configJsonObj.resourcePath || '/UA';
+config.allowAnonymous = configJsonObj.allowAnonymous || true;
+config.disableDiscovery = configJsonObj.disableDiscovery || true;
+config.discoveryServerEndpointUrl = configJsonObj.discoveryServerEndpointUrl || 'opc.tcp://127.0.0.1:4840';
+config.certificateFile = configJsonObj.certificateFile || undefined;
+config.privateKeyFile = configJsonObj.privateKeyFile || undefined;
+config.nodeset_filename = configJsonObj.nodeset_filename || [];
+config.isAuditing = configJsonObj.isAuditing || false;
+config.capabilitiesForMDNS = configJsonObj.capabilitiesForMDNS || [];
+config.defaultSecureTokenLifetime = configJsonObj.defaultSecureTokenLifetime || 300000;
+
+// BuildInfo
 config.buildInfo = {};
 config.buildInfo.productUri = configJsonObj.buildInfo?.productUri || 'SampleServer-productUri';
 config.buildInfo.productName = configJsonObj.buildInfo?.productName || 'SampleServer-productName';
@@ -77,37 +88,49 @@ config.buildInfo.manufacturerName = configJsonObj.buildInfo?.manufacturerName ||
 'SampleServer-manufacturerName';
 config.buildInfo.buildNumber = configJsonObj.buildInfo?.buildNumber || 'v1.0.0';
 config.buildInfo.buildDate = new Date(String(configJsonObj.buildInfo?.buildDate)) || new Date();
-config.buildInfo.softwareVersion = `node-opcua: ${packageJsonObj.dependencies["node-opcua"] || "latest"}`;
+config.buildInfo.softwareVersion = `node-opcua: ${packageJsonObj.dependencies['node-opcua']}`;
+
+// ServerInfo
 config.serverInfo = {};
 config.serverInfo.applicationName = configJsonObj.serverInfo?.applicationName || {
-  "text": "SampleServer-applicationName",
-  "locale": "en-US"
+  'text': 'SampleServer-applicationName',
+  'locale': 'en-US'
 };
-config.serverInfo.applicationUri = configJsonObj.serverInfo?.applicationUri || "urn:SampleServer";
-config.serverInfo.productUri = configJsonObj.serverInfo?.productUri || "SampleServer-productUri";
+config.serverInfo.applicationUri = configJsonObj.serverInfo?.applicationUri || 'urn:SampleServer';
+config.serverInfo.productUri = configJsonObj.serverInfo?.productUri || 'SampleServer-productUri';
 config.serverInfo.applicationType = configJsonObj.serverInfo?.applicationType || ApplicationType.Server;
-config.serverInfo.gatewayServerUri = configJsonObj.serverInfo?.gatewayServerUri || "";
-config.serverInfo.discoveryProfileUri = configJsonObj.serverInfo?.discoveryProfileUri || "";
+config.serverInfo.gatewayServerUri = configJsonObj.serverInfo?.gatewayServerUri || '';
+config.serverInfo.discoveryProfileUri = configJsonObj.serverInfo?.discoveryProfileUri || '';
 config.serverInfo.discoveryUrls = configJsonObj.serverInfo?.discoveryUrls || []
+
+// ServerCapabilities and OperationLimits
 const operationLimits = configJsonObj.serverCapabilities?.operationLimits;
 config.serverCapabilities = new ServerCapabilities({
+  localeIdArray: configJsonObj.serverCapabilities?.localeIdArray || ["en-Us"],
   maxBrowseContinuationPoints: configJsonObj.serverCapabilities?.maxBrowseContinuationPoints || 10,
   maxArrayLength: configJsonObj.serverCapabilities?.maxArrayLength || 1000,
   minSupportedSampleRate: configJsonObj.serverCapabilities?.minSupportedSampleRate || 100,
+  // maxByteStringLength: configJsonObj.serverCapabilities?.maxByteStringLength || undefined,
+  // maxStringLength: configJsonObj.serverCapabilities?.maxStringLength || undefined,
+  maxHistoryContinuationPoints: configJsonObj.serverCapabilities?.maxHistoryContinuationPoints || 10,
   operationLimits: new OperationLimits({
     maxMonitoredItemsPerCall: operationLimits?.maxMonitoredItemsPerCall || 1000,
     maxNodesPerBrowse: operationLimits?.maxNodesPerBrowse || 1000,
+    maxNodesPerHistoryReadData: operationLimits?.maxNodesPerHistoryReadData || 1000,
+    maxNodesPerHistoryReadEvents: operationLimits?.maxNodesPerHistoryReadEvents || 1000,
+    maxNodesPerHistoryUpdateData: operationLimits?.maxNodesPerHistoryUpdateData || 1000,
+    maxNodesPerHistoryUpdateEvents: operationLimits?.maxNodesPerHistoryUpdateEvents || 1000,
+    maxNodesPerMethodCall: operationLimits?.maxNodesPerMethodCall || 100,
+    maxNodesPerNodeManagement: operationLimits?.maxNodesPerNodeManagement || 100,
     maxNodesPerRead: operationLimits?.maxNodesPerRead || 1000,
     maxNodesPerRegisterNodes: operationLimits?.maxNodesPerRegisterNodes || 1000,
     maxNodesPerTranslateBrowsePathsToNodeIds: operationLimits?.maxNodesPerTranslateBrowsePathsToNodeIds || 1000,
-    maxNodesPerWrite: operationLimits?.maxNodesPerWrite || 1000
-  })
+    maxNodesPerWrite: operationLimits?.maxNodesPerWrite || 1000,
+
+  }),
+  serverProfileArray: configJsonObj.serverCapabilities?.serverProfileArray || [],
+  softwareCertificates: [],
 });
-config.allowAnonymous = configJsonObj.allowAnonymous || true;
-config.disableDiscovery = configJsonObj.disableDiscovery || true;
-config.certificateFile = configJsonObj.certificateFile || undefined
-config.privateKeyFile = configJsonObj.privateKeyFile || undefined
-config.nodeset_filename = configJsonObj.nodeset_filename || []
 
 const userManager = {
   isValidUserAsync: isValidUserAsync,
