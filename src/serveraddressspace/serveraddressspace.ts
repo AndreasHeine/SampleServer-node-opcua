f41e8ed5faea23d55b0e94086f7abe9b470cdb19
Andreas <info@andreas-heine.net>
2022-03-01 14:15:02 +0100

    Update serveraddressspace.ts

--- src/serveraddressspace/serveraddressspace.ts
+++ src/serveraddressspace/serveraddressspace.ts
@@ -292,7 +292,7 @@ export const createOwnServerAddressspaceLogic = async (addressSpace: AddressSpac
             value: randomIdx,
             dataType: DataType.UInt32
         })
-    }, 2000)
+    }, 60000)
 
     const ownEventType = namespace.addEventType({
         browseName: 'ownNonExclusiveLimitAlarmType',
