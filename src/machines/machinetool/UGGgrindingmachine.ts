c03f37e12d53d33da24a59e574d141874060a141
Götz Görisch <g.goerisch@vdw.de>
2024-04-22 14:51:55 +0200

    feat: add UGG machine example

---
+++ src/machines/machinetool/UGGgrindingmachine.ts
@@ -0,0 +1,26 @@
+// Copyright (c) 2024 Goetz Goerisch
+//
+//   Licensed under the Apache License, Version 2.0 (the 'License');
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an 'AS IS' BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+import { 
+    coerceLocalizedText, 
+    coerceNodeId,
+    DataType,
+    UAVariable,
+    AddressSpace,
+    NodeId,
+    NodeIdType,
+} from 'node-opcua'
+
+export const createUGGgrindingMachineLogic = async (addressSpace: AddressSpace): Promise<void> => {
+}
