e817c5e837f9659f3d5ae66ba2fe97be5bb43943
Andreas Heine <info@andreas-heine.net>
2025-01-06 16:57:59 +0100

    fix: healthcheck does no request

--- healthcheck.js
+++ healthcheck.js
@@ -3,5 +3,6 @@ const client = nodeopcua.OPCUAClient.create({ endpointMustExist: false });
 const ip = process.env.IP || "localhost";
 const port = process.env.PORT || 4840;
 const url = `opc.tcp://${ip}:${port}`;
-client.connect(url, () => {});
-client.disconnect(() => {});
+client.connect(url, () => {
+    client.disconnect(() => {});
+});
