776dee520b5a4105cf985b46103828460438dd08
Andreas Heine <info@andreas-heine.net>
2025-01-06 17:05:25 +0100

    fix: linting

--- healthcheck.js
+++ healthcheck.js
@@ -4,5 +4,5 @@ const ip = process.env.IP || "localhost";
 const port = process.env.PORT || 4840;
 const url = `opc.tcp://${ip}:${port}`;
 client.connect(url, () => {
-    client.disconnect(() => {});
+  client.disconnect(() => {});
 });
