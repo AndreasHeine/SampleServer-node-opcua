03c4ef5cee2e52dcedfd60508bdf404939b21335
Alexander Inglessi <a.inglessi@vdw.de>
2025-04-17 09:22:18 +0200

    feat: increase PowerOnDuration every minute

--- src/machines/machinetool/machinetool-energy.ts
+++ src/machines/machinetool/machinetool-energy.ts
@@ -146,117 +146,18 @@ export const createMachineToolEnergyLogic = async (
     });
   }, 1000);
 
+  // Inctrease PowerOnDuration every 1 min.
+  setInterval(() => {
+    const powerOnDuration = addressSpace?.findNode(`ns=${idx};i=6333`) as UAVariable;
+    let oldT = powerOnDuration.readValue().value.value;
+    powerOnDuration?.setValueFromSource({
+      value: oldT + 60000,
+      dataType: DataType.Double,
+    });
+  }, 60000);
+
   function getRandomInRange(min: number, max: number): number {
     return (Math.random() * (max - min) + min);
   }
 
-/*   const activeProgramName = addressSpace?.findNode(
-    `ns=${idx};i=6026`,
-  ) as UAVariable;
-  if (activeProgramName !== null) {
-    activeProgramName?.setValueFromSource({
-      value: "Program_2",
-      dataType: DataType.String,
-    });
-  } else {
-    red("activeProgramName does not exist!");
-  }
-
-  const activeProgramNumberInList = addressSpace?.findNode(
-    `ns=${idx};i=6027`,
-  ) as UAVariable;
-  if (activeProgramNumberInList !== null) {
-    activeProgramNumberInList?.setValueFromSource({
-      value: 1,
-      dataType: DataType.UInt16,
-    });
-  } else {
-    red("activeProgramNumberInList does not exist!");
-  }
-
-  const productionActiveProgramStateCurrentState = addressSpace?.findNode(
-    `ns=${idx};i=6028`,
-  ) as UAVariable;
-  if (productionActiveProgramStateCurrentState !== null) {
-    productionActiveProgramStateCurrentState?.setValueFromSource({
-      value: coerceLocalizedText("Running"),
-      dataType: DataType.LocalizedText,
-    });
-  } else {
-    red("productionActiveProgramStateCurrentState does not exist!");
-  }
-
-  // changes CurrentState each 10000 msec from Running to Ended
-  setInterval(() => {
-    const state = addressSpace?.findNode(`ns=${idx};i=6028`) as UAVariable;
-    const stateNumber = addressSpace?.findNode(
-      `ns=${idx};i=6029`,
-    ) as UAVariable;
-    if (state?.readValue().value.value.text === "Running") {
-      state?.setValueFromSource({
-        value: coerceLocalizedText("Ended"),
-        dataType: DataType.LocalizedText,
-      });
-      stateNumber.setValueFromSource({
-        value: coerceNodeId(2),
-        dataType: DataType.NodeId,
-      });
-    } else {
-      state?.setValueFromSource({
-        value: coerceLocalizedText("Running"),
-        dataType: DataType.LocalizedText,
-      });
-      stateNumber.setValueFromSource({
-        value: coerceNodeId(1),
-        dataType: DataType.NodeId,
-      });
-    }
-  }, 10000);
-
-  // increments the value of FeedOverride by 5 each sec
-  let override = 50;
-  setInterval(() => {
-    override += 5;
-    if (override > 120) {
-      override = 50;
-    }
-    const feedOverride = addressSpace?.findNode(
-      `ns=${idx};i=6017`,
-    ) as UAVariable;
-    feedOverride.setValueFromSource({
-      value: override,
-      dataType: DataType.Double,
-    });
-  }, 1000);
-
-  const spindleOverride = addressSpace?.findNode(
-    `ns=${idx};i=6070`,
-  ) as UAVariable;
-  spindleOverride?.setValueFromSource({
-    value: 100,
-    dataType: DataType.Double,
-  });
-
-  // writing ExtensionObject (Range)
-  const spindleEURange = addressSpace?.constructExtensionObject(
-    new NodeId(NodeIdType.NUMERIC, 884, 0),
-    {
-      low: 60.0,
-      high: 100.0,
-    },
-  );
-  const spindleOverrideRange = addressSpace?.findNode(
-    `ns=${idx};i=6072`,
-  ) as UAVariable;
-  spindleOverrideRange?.setValueFromSource({
-    value: spindleEURange,
-    dataType: DataType.ExtensionObject,
-  });
-
-  // writing a enum
-  const channel1 = addressSpace?.findNode(`ns=${idx};i=6061`) as UAVariable;
-  channel1.setValueFromSource({
-    value: 1,
-    dataType: DataType.Int32,
-  }); */
 };
