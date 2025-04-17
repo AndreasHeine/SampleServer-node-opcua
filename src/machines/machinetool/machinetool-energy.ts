621acf7584b691961e3f51c4a4653e3c55687de7
Alexander Inglessi <a.inglessi@vdw.de>
2025-04-17 08:33:12 +0200

    feat: create simulations for some energy parameters

--- src/machines/machinetool/machinetool-energy.ts
+++ src/machines/machinetool/machinetool-energy.ts
@@ -22,6 +22,7 @@ import {
   AddressSpace,
   NodeId,
   NodeIdType,
+  LocalizedText,
 } from "node-opcua";
 import { red } from "../../utils/log";
 
@@ -37,6 +38,117 @@ export const createMachineToolEnergyLogic = async (
   const mtoolIdx = addressSpace?.getNamespaceIndex(
     "http://opcfoundation.org/UA/MachineTool/",
   );
+
+  // changes CurrentState each 10000 msec from Running to Interrupted
+  setInterval(() => {
+    const state = addressSpace?.findNode(`ns=${idx};i=6144`) as UAVariable;
+    const stateId = addressSpace?.findNode(
+      `ns=${idx};i=6145`,
+    ) as UAVariable;
+    const stateNumber = addressSpace?.findNode(
+      `ns=${idx};i=6146`,
+    ) as UAVariable;
+    if (state?.readValue().value.value.text === "Running") {
+      state?.setValueFromSource({
+        value: new LocalizedText({ text: "Interrupted", locale: "en" }),
+        dataType: DataType.LocalizedText,
+      });
+      stateId.setValueFromSource({
+        value: coerceNodeId(`ns=${mtoolIdx};i=5040`),
+        dataType: DataType.NodeId,
+      });
+      stateNumber.setValueFromSource({
+        value: 3,
+        dataType: DataType.UInt32,
+      });
+    } else {
+      state?.setValueFromSource({
+        value: new LocalizedText({ text: "Running", locale: "en" }),
+        dataType: DataType.LocalizedText,
+      });
+      stateId.setValueFromSource({
+        value: coerceNodeId(`ns=${mtoolIdx};i=5041`),
+        dataType: DataType.NodeId,
+      });
+      stateNumber.setValueFromSource({
+        value: 1,
+        dataType: DataType.UInt32,
+      });
+    }
+  }, 10000);
+
+  // Simulate voltage and current fluctuiations every 1 sec.
+  setInterval(() => {
+    let v1 = getRandomInRange(225, 235);
+    let v2 = getRandomInRange(225, 235);
+    let v3 = getRandomInRange(225, 235);
+    let c1 = getRandomInRange(1.9, 2.1);
+    let c2 = getRandomInRange(1.9, 2.1);
+    let c3 = getRandomInRange(1.9, 2.1);
+    let p1 = v1 * c1;
+    let p2 = v2 * c2;
+    let p3 = v3 * c3;
+    let p = p1 + p2 + p3;
+    let e = p * 0.000278; // Wh
+    const newV = {
+        L1: v1,
+        L2: v2,
+        L3: v3
+      };
+    const newC = {
+        L1: c1,
+        L2: c2,
+        L3: c3
+      };
+    const newP = {
+        L1: p1,
+        L2: p2,
+        L3: p3
+      };
+    const voltage = addressSpace?.findNode(`ns=${idx};i=6196`) as UAVariable;
+    voltage?.setValueFromSource({
+      value: newV,
+      dataType: DataType.ExtensionObject
+    });
+    const current = addressSpace?.findNode(`ns=${idx};i=6212`) as UAVariable;
+    current?.setValueFromSource({
+      value: newC,
+      dataType: DataType.ExtensionObject
+    });
+    const power = addressSpace?.findNode(`ns=${idx};i=6147`) as UAVariable;
+    power?.setValueFromSource({
+      value: newP,
+      dataType: DataType.ExtensionObject
+    });
+    const energyImport = addressSpace?.findNode(`ns=${idx};i=6164`) as UAVariable;
+    let oldE = energyImport.readValue().value.value;
+    energyImport?.setValueFromSource({
+      value: oldE + e,
+      dataType: DataType.Double,
+    });
+  }, 1000);
+
+  // Simulate water flow fluctuiation every 1 sec.
+  setInterval(() => {
+    const waterFlow = addressSpace?.findNode(`ns=${idx};i=6256`) as UAVariable;
+    waterFlow?.setValueFromSource({
+      value: getRandomInRange(1.1, 1.3),
+      dataType: DataType.Float,
+    });
+  }, 1000);
+
+  // Simulate air flow fluctuiation every 1 sec.
+  setInterval(() => {
+    const airFlow = addressSpace?.findNode(`ns=${idx};i=6266`) as UAVariable;
+    airFlow?.setValueFromSource({
+      value: getRandomInRange(1.4, 1.6),
+      dataType: DataType.Float,
+    });
+  }, 1000);
+
+  function getRandomInRange(min: number, max: number): number {
+    return (Math.random() * (max - min) + min);
+  }
 
 /*   const activeProgramName = addressSpace?.findNode(
     `ns=${idx};i=6026`,
